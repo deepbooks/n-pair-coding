7ff64d134008f3d6c280c5caad0421a4ab66dc6f
paranlee <p4ranlee@gmail.com>
2021-06-29 18:22:14 +0900

    Add multiplayer coding features.

--- server.js
+++ server.js
@@ -14,17 +14,20 @@ const app = express();
 }; */
 
 // Create Server using the app and bind it to a port
-//https.createServer(options, app).listen(4000)
-const server = http.createServer(app).listen(8080);
+/* https.createServer(options, app).listen(4000); */
+const server = http.createServer(app).listen(8081);
 
 // Static file serving
-app.use("/",express.static("./"));
+app.use("/", express.static("./"));
 
 // Bind socket.io to the server
 const io = require('socket.io')(server);
 
+const room = "default";
+
 // When a new socket connects
 io.on('connection', socket => {
+    socket.join(room);
     // Create terminal
     const term = pty.spawn('sh', [], {
         name: 'xterm-color',
@@ -33,26 +36,37 @@ io.on('connection', socket => {
         cwd: process.env.HOME,
         env: process.env
     });
-
-    // Listen on the terminal for output and send it to the client
-    term.on('data', data => {
-        console.log(data);
-        io.emit('output', data);
-    });
-
-    // Listen on the client and send any input to the terminal
-    socket.on('input', data => {
-        term.write(data);
-    });
-
-    // Listen for a resize request and update the terminal size
-    socket.on('resize', data => {
-        term.resize(data[0], data[1]);
-    });
+        
+    io.emit('connected', JSON.stringify(sids2array(io)));
 
     // When socket disconnects, destroy the terminal
     socket.on("disconnect", () => {
         term.destroy();
-        console.log("bye");
+        io.emit("disconnected");
+        console.log(`${socket.id}: bye`);
     });
+
+    // Listen on the terminal for output and send it to the client
+    term.on('data', data => {
+        const json = JSON.stringify({ sid: socket.id, data });
+        // socket.broadcast.to(room).emit('output', json);
+        io.emit('output', json);
+    });
+
+    // Listen on the client and send any input to the terminal
+    socket.on('input', data => term.write(data));
+
+    // Listen for a resize request and update the terminal size
+    socket.on('resize', data => term.resize(data[0], data[1]));
 });
+
+function sids2array(io) {
+    const sids = io.of("/").adapter.sids;
+
+    const array = [];
+    for (let [sid, room] of sids)
+        array.push(sid);
+
+    console.log(array);
+    return array;
+}
